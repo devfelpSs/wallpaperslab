8625f2ed1e0c4c6f019515e1e2570027dd8f7255
devfelpSs <devfelps@outlook.com>
2023-03-16 12:28:34 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/wallpaperslab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wallpaperslab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaperslab/shared/widgets/my_app.dart';
+
+void main (){
+  runApp(const MyApp());
+}
