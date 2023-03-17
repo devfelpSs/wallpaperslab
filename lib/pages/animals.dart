4fc70511476af4947e280a7337a522a9626ec4b3
devfelpSs <devfelps@outlook.com>
2023-03-17 12:02:34 -0300

    Criando paginas das imagens

---
+++ lib/pages/animals.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AnimalsImages extends StatefulWidget {
+  const AnimalsImages({super.key});
+
+  @override
+  State<AnimalsImages> createState() => _AnimalsImagesState();
+}
+
+class _AnimalsImagesState extends State<AnimalsImages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/pages/nature.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class NatureImages extends StatefulWidget {
+  const NatureImages({super.key});
+
+  @override
+  State<NatureImages> createState() => _NatureImagesState();
+}
+
+class _NatureImagesState extends State<NatureImages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
