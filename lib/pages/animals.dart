ad756f1b1ba88a700b566359f5443dfb59ef5382
devfelpSs <devfelps@outlook.com>
2023-03-24 12:13:57 -0300

    add more one section to app

--- lib/pages/animals.dart
+++ lib/pages/animals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaperslab/shared/widgets/app_images.dart';
 
 class AnimalsImages extends StatefulWidget {
   const AnimalsImages({super.key});
@@ -10,6 +11,27 @@ class AnimalsImages extends StatefulWidget {
 class _AnimalsImagesState extends State<AnimalsImages> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Card(child: Image.network(AppImages.animal1)),
+                Card(child: Image.network(AppImages.animal2)),
+                Card(child: Image.network(AppImages.animal3)),
+                Card(child: Image.network(AppImages.animal4)),
+                Card(child: Image.network(AppImages.animal5)),
+                Card(child: Image.network(AppImages.animal6)),
+                Card(child: Image.network(AppImages.animal7)),
+                Card(child: Image.network(AppImages.animal8)),
+                Card(child: Image.network(AppImages.animal9)),
+                Card(child: Image.network(AppImages.animal10)),
+              ],
+            ))
+        ],
+      ),
+    );
   }
 }
