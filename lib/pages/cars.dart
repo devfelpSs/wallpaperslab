b210f08c191f003a8b804c6482151a59c0995ed6
devfelpSs <devfelps@outlook.com>
2023-03-24 11:16:57 -0300

    trying to fix errors

--- lib/pages/cars.dart
+++ lib/pages/cars.dart
@@ -11,34 +11,28 @@ class CarsImages extends StatefulWidget {
 class _CarsImagesState extends State<CarsImages> {
   @override
   Widget build(BuildContext context) {
-    try {
-      return Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                children: [
-                  Card(child: Image.asset(AppImages.car1),),
-                  Card(child: Image.asset(AppImages.car2),),
-                  Card(child: Image.asset(AppImages.car3),),
-                  Card(child: Image.asset(AppImages.car4),),
-                  Card(child: Image.asset(AppImages.car5),),
-                  Card(child: Image.asset(AppImages.car6),),
-                  Card(child: Image.asset(AppImages.car7),),
-                  Card(child: Image.asset(AppImages.car8),),
-                  Card(child: Image.asset(AppImages.car9),),
-                  Card(child: Image.asset(AppImages.car10),),
-                ],
-              )
-            ),
-          ],
-        ),
-      );
-    } catch (e) {
-      print('Erro ao carregar imagens: $e');
-      return Container();
-    }
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Card(child: Image.asset(AppImages.car1,)),
+                Card(child: Image.asset(AppImages.car2,)),
+                Card(child: Image.asset(AppImages.car3,)),
+                Card(child: Image.asset(AppImages.car4,)),
+                Card(child: Image.asset(AppImages.car5,)),
+                Card(child: Image.asset(AppImages.car6,)),
+                Card(child: Image.asset(AppImages.car7,)),
+                Card(child: Image.asset(AppImages.car8,)),
+                Card(child: Image.asset(AppImages.car9,)),
+                Card(child: Image.asset(AppImages.car10,)),
+              ],
+            )
+          ),
+          Expanded(flex: 3, child: Container())
+      ],),
+    );
   }
 }
