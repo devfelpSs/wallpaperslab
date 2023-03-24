f288d39fe7aa9a67a3fa78bebc1b2f7b704e23de
devfelpSs <devfelps@outlook.com>
2023-03-24 12:09:39 -0300

    add more images

--- lib/shared/widgets/app_images.dart
+++ lib/shared/widgets/app_images.dart
@@ -2,16 +2,16 @@
 
 class AppImages {
   //Animals:
-  static String get animal1 => "/lib/images/animal1.jpg";
-  static String get animal2 => "/lib/images/animal2.jpg";
-  static String get animal3 => "/lib/images/animal3.jpg";
-  static String get animal4 => "/lib/images/animal4.jpg";
-  static String get animal5 => "/lib/images/animal5.jpg";
-  static String get animal6 => "/lib/images/animal6.jpg";
-  static String get animal7 => "/lib/images/animal7.jpg";
-  static String get animal8 => "/lib/images/animal8.jpg";
-  static String get animal9 => "/lib/images/animal9.jpg";
-  static String get animal10 => "/lib/images/animal10.jpg";
+  static String get animal1 => "https://images.unsplash.com/photo-1613318282885-2168d00a8d2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+  static String get animal2 => "https://images.unsplash.com/photo-1591824438708-ce405f36ba3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+  static String get animal3 => "https://images.unsplash.com/photo-1535083783855-76ae62b2914e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80";
+  static String get animal4 => "https://images.unsplash.com/photo-1568265112889-c9d3fc50a281?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+  static String get animal5 => "https://images.unsplash.com/photo-1484406566174-9da000fda645?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80";
+  static String get animal6 => "https://images.unsplash.com/photo-1543260775-945c562403b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=664&q=80";
+  static String get animal7 => "https://images.unsplash.com/photo-1619038779484-a27362fb82cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80";
+  static String get animal8 => "https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+  static String get animal9 => "https://images.unsplash.com/photo-1554490828-442467b562dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+  static String get animal10 => "https://images.unsplash.com/photo-1557008075-7f2c5efa4cfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=697&q=80";
   //Cars:
   static String get car1 => "https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
   static String get car2 => "https://images.unsplash.com/photo-1544829099-13af78aa41b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
